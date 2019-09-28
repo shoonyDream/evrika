9a4ad0e140269b13fc4a4f976eda9a4917214109
ShoonY <5002745@mail.ru>
2019-09-28 10:42:37 +0300

    add scaffold

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'normalize.css';
+import './styles/style.scss';
+
+// import 'jquery.js';
+import './scripts/slider.js';
+
+console.log('hello world');
---
+++ src/scripts/slider.js
@@ -0,0 +1,20 @@
+import Swiper from 'swiper/js/swiper.min.js';
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+});
